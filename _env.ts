6372e8fef42d3a63f1a9fe8343fef29008defcd4
Charlotte Som <charlotte@som.codes>
2024-10-26 03:27:00 +0000

    add css hot reloading

--- _env.ts
+++ _env.ts
@@ -11,3 +11,26 @@ declare global {
     readonly env: ImportMetaEnv;
   }
 }
+
+if (import.meta.env.LIVE_RELOAD) {
+  new EventSource("/esbuild").addEventListener("change", e => {
+    const { added, removed, updated } = JSON.parse(e.data);
+
+    if (!added.length && !removed.length && updated.length >= 1) {
+      // @ts-ignore eyyy dont worry about it
+      for (const link of document.getElementsByTagName("link")) {
+        const url = new URL(link.href);
+
+        if (url.host === location.host && url.pathname === updated[0]) {
+          const next = link.cloneNode();
+          next.href = updated[0] + "?" + Math.random().toString(36).slice(2);
+          next.onload = () => link.remove();
+          link.parentNode.insertBefore(next, link.nextSibling);
+          return;
+        }
+      }
+    }
+
+    location.reload();
+  });
+}
